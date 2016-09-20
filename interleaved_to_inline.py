25686118bf887c3007a4fe26f63bdbcc14457654
Giovanni Mascellani <gio@debian.org>
2016-09-20 22:26:25 +0200

    First step towards inserting chords in text.

--- interleaved_to_inline.py
+++ interleaved_to_inline.py
@@ -22,12 +22,31 @@ def categorize_line(line):
         return 'CHORDS'
     return 'TEXT'
 
+def canonicalize_chord(chord):
+    chord = list(chord)
+    chord[0] = chord[0].lower()
+    if chord[0] in ITALIAN_CHORDS:
+        chord[0] = ENGLISH_CHORDS[ITALIAN_CHORDS.index(chord[0])]
+    return tuple(chord)
+
+def apply_chords_to_text(chords, text):
+    subst = []
+    for match in CHORD_RE.finditer(chords):
+        chord = canonicalize_chord(match.groups())
+        pos = match.start()
+        subst.append((chord, pos))
+    subst.reverse()
+    for chord, pos in subst:
+        text = text[:pos] + "[" + "".join(chord) + "]" + text[pos:]
+    return text
+
 def main():
     sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout.detach())
     sys.stdin = codecs.getreader(locale.getpreferredencoding())(sys.stdin.detach())
     interleaved = [x.strip() for x in sys.stdin.readlines()]
-    for line in interleaved:
-        print(categorize_line(line), line)
+    #for line in interleaved:
+    #    print(categorize_line(line), line)
+    print(apply_chords_to_text(interleaved[2], interleaved[3]))
 
 if __name__ == '__main__':
     main()
